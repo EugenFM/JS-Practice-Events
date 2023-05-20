4e65eac78c8dabcd48bb369254ac1c537fdca84c
EugenFM <eugenfm22@gmail.com>
2023-05-20 14:40:17 -0500

    Added toggle switch event

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
+var themeSwitchInput = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var heading1 = document.querySelector("h1");
+var body = document.querySelector("body");
 
 fans.addEventListener("mouseover", function () {
     fans.innerText = "123K";
@@ -11,6 +15,18 @@ fish.addEventListener("mouseover", function () {
 });
 
 pets.addEventListener("mouseover", function () {
-pets.innerText = "20K";
+    pets.innerText = "20K";
+});
+
+themeSwitchInput.addEventListener("click", function () {
+    if (body.classList.contains("light")) {
+        body.classList.remove("light");
+        ball.classList.remove("move-right");
+        heading.innerText = "Stealth Quincy";
+    } else {
+body.classList.add("light");
+ball.classList.add("move-right");
+heading1.innerText = "Party Quincy";
+    }
 });
 
